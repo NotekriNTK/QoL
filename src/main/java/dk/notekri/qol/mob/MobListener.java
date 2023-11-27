ce99931906735774e0a87a9a96d477cd38054b20
Notekri <NotekriNTK@gmail.com>
2023-11-27 23:46:32 +0100

    Enderman

--- src/main/java/dk/notekri/qol/mob/MobListener.java
+++ src/main/java/dk/notekri/qol/mob/MobListener.java
@@ -1,9 +1,17 @@
 package dk.notekri.qol.mob;
 
 import dk.notekri.qol.core.Handler;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.WorldType;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.block.Biome;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.Enderman;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
@@ -14,56 +22,46 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.joml.Random;
 
 import java.util.Arrays;
 import java.util.UUID;
 
 public class MobListener implements Listener {
-    boolean isSpawned = false;
     private Handler handler;
-
-
-
     public MobListener(Handler handler){
         this.handler = handler;
     }
 
     @EventHandler
     public void onMobSpawn(CreatureSpawnEvent event){
-        if (!(event.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.COMMAND))){
+        if (!(event.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.COMMAND) || event.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.NATURAL))){
             return;
         }
-        if (!event.getEntityType().equals(EntityType.ZOMBIE))return;
-        //if (isSpawned) return;
 
-        AttributeModifier modifier = new AttributeModifier(
-                UUID.randomUUID(),  // Give it a unique name
-                "HoeDamage",
-                2.0d,// Set the amount of damage here
-                AttributeModifier.Operation.ADD_NUMBER,
-                EquipmentSlot.HAND
-                );
+        if (!event.getEntityType().equals(EntityType.ENDERMAN))return;
+        Enderman enderman = (Enderman)event.getEntity();
+        Biome biome = enderman.getLocation().getBlock().getBiome();
+        if (!(biome.equals(Biome.END_BARRENS)
+                ||biome.equals(Biome.END_HIGHLANDS)
+                ||biome.equals(Biome.END_MIDLANDS))){
+            return;}
 
-        Zombie zombie = (Zombie)event.getEntity();
-        zombie.setMaxHealth(40d);
-        zombie.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE,1000,2) ,true);
-        zombie.addPotionEffect(new PotionEffect(PotionEffectType.SPEED,1000,1) ,true);
-        zombie.getEquipment().setHelmet(new ItemStack(Material.ORANGE_GLAZED_TERRACOTTA,1));
+        Random random = new Random();
+        int x = random.nextInt(250);
+        if (x != 0){
+            return;
+        }
+        BlockData data = Bukkit.createBlockData(Material.AMETHYST_CLUSTER);
+        enderman.setCarriedBlock(data);
+        enderman.setMaxHealth(60d);
+        enderman.setHealth(60d);
+        enderman.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE,1000,2));
+        enderman.addPotionEffect(new PotionEffect(PotionEffectType.SPEED,1000,2));
+        enderman.setGlowing(true);
+        enderman.getWorld().playSound(enderman, Sound.ENTITY_WITHER_SHOOT, 2, 2);
+        enderman.setRemoveWhenFarAway(true);
 
-        ItemStack hand = new ItemStack(Material.DIAMOND_HOE,1);
-        ItemMeta meta = hand.getItemMeta();
-        meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE,modifier);
-        String[] lore = {"§7 Ouch"};
-        meta.setLore(Arrays.asList(lore));
-        hand.setItemMeta(meta);
-
-        zombie.getEquipment().setItemInMainHand(hand);
-        zombie.getEquipment().setDropChance(EquipmentSlot.HEAD,100f);
-        zombie.getEquipment().setDropChance(EquipmentSlot.HAND,50f);
-        zombie.clearLootTable();
-
-        zombie.setInvisible(true);
-        isSpawned = true;
     }
 
 }
